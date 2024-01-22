fb14f98b0da191dae941e232e558dc13dbfe0961
Huy Phan <phan.q.huy21@gmail.com>
2024-01-22 18:16:32 -0500

    completed quick start
    + example routes run well
    + axios config fixed

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,20 +1,98 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-// import axios from "client/util/axios";
-import axios from "axios";
+import { usePlaidLink } from "react-plaid-link";
+import axios from "./util/axios";
 
-const page = () => {
-  const [data, setData] = useState("");
+const Page = () => {
+  const [linkToken, setLinkToken] = useState();
+  const [publicToken, setPublicToken] = useState();
+  const [account, setAccount] = useState();
+  const [acc, setAcc] = useState();
 
   useEffect(() => {
-    axios.get("http://localhost:8080/api/home").then((res) => {
-      setData(res.data.message);
-      console.log(res.data);
-    });
+    // Step 1: Get the link token from the server
+    const createLinkToken = async () => {
+      const res = await axios.post("/create_link_token");
+      setLinkToken(res.data.link_token);
+    };
+    createLinkToken();
   }, []);
 
-  return <div>{data}</div>;
+  // Step 2: use the link token to initialize Plaid Link, on success will provide a temporary public token
+  const { open, ready } = usePlaidLink({
+    token: linkToken,
+    onSuccess: async (public_token, metadata) => {
+      // save the public token
+      setPublicToken(public_token);
+      console.log("Public token saved successfully as: ", public_token);
+      console.log("Here is the metadata:", metadata);
+
+      // Step 3: send public token to server and exchange it for an access token
+      // Send the public token to the server, server will send access token back via the response
+      const accessToken = await axios.post("/exchange_public_token", {
+        public_token: public_token,
+      });
+      console.log("Access token: ", accessToken.data.accessToken);
+
+      const accounts = await axios.post("/accounts", {
+        access_token: accessToken.data.accessToken,
+      });
+
+      console.log("accounts data: ", accounts.data);
+      setAcc(accounts.data);
+
+      // const auth = await axios.post("/auth", {
+      //   access_token: accessToken.data.accessToken,
+      // });
+      // console.log("auth data ", auth.data);
+      // setAccount(auth.data.numbers.ach[0]);
+    },
+  });
+
+  return publicToken ? (
+    // account && (
+    //   <>
+    //     <h1>Account number: {account.account}</h1>
+    //     <h1>Account routing: {account.routing}</h1>
+    //   </>
+    <pre>{JSON.stringify(acc, null, 2)}</pre>
+  ) : (
+    <button onClick={() => open()} disabled={!ready}>
+      Connect a bank account
+    </button>
+  );
 };
 
-export default page;
+// once you have the public token, it will be passed to a new component
+// this component will send the public token to the server and exchange
+// it for an access token.
+
+// const PlaidAuth = ({publicToken}) => {
+//   const [account, setAccount] = useState();
+
+//   // Step 3: send public token to server and exchange it for an access token
+//   useEffect(() => {
+//     const exchangePublicToken = async () => {
+//       // Send the public token to the server, server will send access token back via the response
+//       const accessToken = await axios.post("/exchange_public_token", {public_token: publicToken});
+//       console.log("Access token: ", accessToken.data.accessToken);
+
+//       const auth = await axios.post("/auth", {
+//         access_token: accessToken.data.accessToken,
+//       });
+//       console.log("auth data ", auth.data);
+//       setAccount(auth.data.numbers.ach[0]);
+//     };
+//     exchangePublicToken();
+//   }, []);
+
+//   return account && (
+//     <>
+//       <h1>Account number: {account.account}</h1>
+//       <h1>Account routing: {account.routing}</h1>
+//     </>
+//   )
+// }
+
+export default Page;
--- server/index.js
+++ server/index.js
@@ -1,15 +1,122 @@
-const express = require('express');
+import { Configuration, PlaidApi, PlaidEnvironments } from "plaid";
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import "dotenv/config";
+
 const app = express();
 const PORT = 8080;
-const cors = require('cors');
 
 app.use(cors());
+app.use(bodyParser.json());
+
+const configuration = new Configuration({
+  basePath: PlaidEnvironments.sandbox, // dictates which plaid api env you will access
+  baseOptions: {
+    headers: {
+      "PLAID-CLIENT-ID": process.env.CLIENT_ID,
+      "PLAID-SECRET": process.env.SECRET,
+    },
+  },
+});
+
+const plaidClient = new PlaidApi(configuration);
+
+/* 
+  Plaid Flow:
+    1. send request from your server to plaid to create a link token via /link/token/create
+    2. from your client, use the link token to initialize Plaid Link, on success will provide a 
+        temporary public token which you will send to your server
+    3. after your server gets the public token, exchange it for an access token via another request to plaid
+    4. use the access token to make requests to plaid on behalf of the user
+    5. OPTIONAL: store the access token in your database for future use
+*/
+
+// Step 1: Create Link Token
+app.post("/create_link_token", async (request, response) => {
+  const plaidRequest = {
+    user: {
+      client_user_id: "user",
+    },
+    client_name: "Plaid Test App",
+    products: ["auth"],
+    language: "en",
+    redirect_uri: "http://localhost:3000/",
+    country_codes: ["US"],
+  };
+  try {
+    const createTokenResponse = await plaidClient.linkTokenCreate(plaidRequest);
+    response.json(createTokenResponse.data);
+  } catch (error) {
+    // handle error
+    console.log(error);
+    response.status(500).send("Unable to create link token. Try again.");
+  }
+});
+
+// Step 2: Initialize Plaid Link from Client Side
+
+// Step 3: Exchange Public Token for Access Token
+app.post("/exchange_public_token", async (req, res, next) => {
+  const publicToken = req.body.public_token;
+
+  try {
+    // server makes request to plaid and exchanges the public token for an access token
+    // the response contains the access token
+    const plaidResponse = await plaidClient.itemPublicTokenExchange({
+      public_token: publicToken,
+    });
+
+    // These values should be saved to a persistent database and
+    // associated with the currently signed-in user
+
+    const accessToken = plaidResponse.data.access_token;
+    const itemID = plaidResponse.data.item_id;
+
+    // send access token to client
+    res.json({ accessToken });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Unable to exchange public token. Try again.");
+  }
+});
+
+// Step 4: Use the access token to make requests to plaid on behalf of the user
+app.post("/auth", async (request, response) => {
+  try {
+    const access_token = request.body.access_token;
+    const plaidRequest = {
+      access_token: access_token,
+    };
+    const plaidResponse = await plaidClient.authGet(plaidRequest);
+    // prettyPrintResponse(plaidResponse);
+    response.json(plaidResponse.data);
+  } catch (e) {
+    console.log(e)
+    response.status(500).send("failed");
+  }
+});
+
+// request to accounts/get
+app.post("/accounts", async (request, response, next) => {
+  try {
+    const access_token = request.body.access_token
+    const plaidRequest = {
+      access_token: access_token
+    }
+    const plaidResponse = await plaidClient.accountsBalanceGet(plaidRequest)
+    response.json(plaidResponse.data)
+  } catch (error) {
+    console.log(error)
+    response.status(500).send("failed")
+  }
+})
 
 // example express route
 app.get("/api/home", (req, res) => {
-    res.json({message: "Welcome to the home page!"})
-})
+  res.json({ message: "Welcome to the home page!" });
+});
 
 app.listen(PORT, () => {
-    console.log("server is running on port ${PORT}")
-})
+  console.log(`server is running on port ${PORT}`);
+});
